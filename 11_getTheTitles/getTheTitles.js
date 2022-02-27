92e9343bba8f03176253d229ee564f88ebd5bd76
Diegoo11 <ynoacamino@gmail.com>
2022-02-27 11:56:35 -0500

    11

---
+++ 11_getTheTitles/getTheTitles.js
@@ -0,0 +1,10 @@
+const getTheTitles = function(uwu) {
+    let newarray =[]
+    for(i=0; i<uwu.length; i++){
+        newarray[i] = uwu[i].title
+    }
+    return newarray
+};
+
+// Do not edit below this line
+module.exports = getTheTitles;
